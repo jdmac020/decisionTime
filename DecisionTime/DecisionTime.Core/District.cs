595d6b09a47a2a9c46f7a69b6285dd69af3c3e74
jdmac020 <john.macgregor@gmail.com>
2019-05-07 19:46:30 -0400

    Districts have lists of citizens

--- DecisionTime/DecisionTime.Core/District.cs
+++ DecisionTime/DecisionTime.Core/District.cs
@@ -6,5 +6,11 @@ namespace DecisionTime.Core
 {
     public class District
     {
+        public List<Citizen> Citizens { get; set; }
+
+        public District()
+        {
+            Citizens = new List<Citizen>();
+        }
     }
 }
---
+++ DecisionTime/DecisionTime.CoreTests/DistrictShould.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using Shouldly;
+using DecisionTime.Core;
+using System.Collections.Generic;
+
+namespace DecisionTime.CoreTests
+{
+    public class DistrictShould
+    {
+        [Fact]
+        public void HaveListOfCitizens()
+        {
+            var district = new District();
+
+            district.Citizens.ShouldBeOfType<List<Citizen>>();
+        }
+    }
+}
