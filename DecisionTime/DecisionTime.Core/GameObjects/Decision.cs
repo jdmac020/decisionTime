edf5067170d1f2a2f5d2b9bdbe5e9356134af5a9
jdmac020 <john.macgregor@gmail.com>
2019-05-25 20:08:23 -0400

    Decisions start with value; can have an option added

---
+++ DecisionTime/DecisionTime.Core/GameObjects/Decision.cs
@@ -0,0 +1,24 @@
+﻿using DecisionTime.Core.Other;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecisionTime.Core.GameObjects
+{
+    public class Decision
+    {
+        public double Value;
+        public List<DecisionOption> Options { get; set; }
+
+        public Decision(double value)
+        {
+            Value = value;
+            Options = new List<DecisionOption>();
+        }
+        
+        public void AddOptions(DecisionOption decisionOption)
+        {
+            Options.Add(decisionOption);
+        }
+    }
+}
---
+++ DecisionTime/DecisionTime.CoreTests/DecisionShould.cs
@@ -0,0 +1,30 @@
+﻿using DecisionTime.Core.GameObjects;
+using DecisionTime.Core.Other;
+using Shouldly;
+using Xunit;
+
+namespace DecisionTime.CoreTests
+{
+    public class DecisionShould
+    {
+        [Fact]
+        public void BeCreatedWithValue()
+        {
+            var testValue = .5;
+            var decision = new Decision(testValue);
+
+            decision.Value.ShouldBe(testValue);
+        }
+
+        [Fact]
+        public void HaveOptionsAddedToIt()
+        {
+            var testValue = .5;
+            var decision = new Decision(testValue);
+
+            decision.AddOptions(new DecisionOption());
+
+            decision.Options.Count.ShouldBe(1);
+        }
+    }
+}
