c0cd0f5a8bf010f7bd488630441ec8b3638c5da9
jdmac020 <john.macgregor@gmail.com>
2019-05-27 08:42:37 -0400

    DRY'd up CitizenShould

--- DecisionTime/DecisionTime.CoreTests/CitizenShould.cs
+++ DecisionTime/DecisionTime.CoreTests/CitizenShould.cs
@@ -8,30 +8,35 @@ namespace DecisionTime.CoreTests
 {
     public class CitizenShould
     {
+        private const string testNameBob = "Bob";
+        private Citizen _citizen;
+
+        private void CreateIndifferentCitizenNamedBob()
+        {
+            _citizen = new Citizen(testNameBob);
+        }
+
         [Fact]
         public void HaveName()
         {
-            var testName = "Bob";
-            var citizen = new Citizen(testName);
+            CreateIndifferentCitizenNamedBob();
 
-            citizen.Name.ShouldBe(testName);
+            _citizen.Name.ShouldBe(testNameBob);
         }
 
         [Fact]
         public void StartWithIndifferentAttitude()
         {
-            var testName = "Bob";
-            var citizen = new Citizen(testName);
+            CreateIndifferentCitizenNamedBob();
 
-            citizen.CurrentAttitude.ShouldBe(Attitude.Indifferent);
+            _citizen.CurrentAttitude.ShouldBe(Attitude.Indifferent);
         }
 
         [Fact]
         public void BeAbleToCreatedWithSpecifiedAttitude()
         {
-            var testName = "Bob";
             var desiredAttitude = Attitude.Favorable;
-            var citizen = new Citizen(testName, desiredAttitude);
+            var citizen = new Citizen(testNameBob, desiredAttitude);
             
             citizen.CurrentAttitude.ShouldBe(desiredAttitude);
         }
@@ -39,28 +44,26 @@ namespace DecisionTime.CoreTests
         [Fact]
         public void UpdateAttitude()
         {
-            var testName = "Bob";
-            var citizen = new Citizen(testName);
+            CreateIndifferentCitizenNamedBob();
 
-            citizen.UpdateAttitude(Attitude.Favorable);
+            _citizen.UpdateAttitude(Attitude.Favorable);
 
-            citizen.CurrentAttitude.ShouldBe(Attitude.Favorable);
+            _citizen.CurrentAttitude.ShouldBe(Attitude.Favorable);
         }
 
         [Fact]
         public void UpdateAttitudeWithDecision()
         {
-            var testName = "Bob";
-            var citizen = new Citizen(testName);
+            CreateIndifferentCitizenNamedBob();
             var goodDecision = new Decision(1, "Taco Tuesday");
             var option = new DecisionOption("Yes", OptionTypes.Good);
             option.IsSelected = true;
             goodDecision.AddOptions(option);
             goodDecision.IsResolved = true;
 
-            citizen.UpdateAttitude(goodDecision);
+            _citizen.UpdateAttitude(goodDecision);
 
-            citizen.CurrentAttitude.ShouldBe(Attitude.Favorable);
+            _citizen.CurrentAttitude.ShouldBe(Attitude.Favorable);
         }
     }
 }
